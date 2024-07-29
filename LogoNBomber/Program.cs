ab4a36f3e094f4c3173dd68de65a0769ab28fd3b
emreevcin <emreevcin35@gmail.com>
2024-07-29 17:35:59 +0300

    presentation

--- LogoNBomber/Program.cs
+++ LogoNBomber/Program.cs
@@ -22,50 +22,102 @@ namespace LogoNBomber
             var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
             var httpClient = httpClientFactory.CreateClient();
 
-            var testLoginLogoutScenario = new TestLoginLogoutScenario();
-            var testActivityScenario = new TestActivityScenario();
+            //var testLoginLogoutScenario = new TestLoginLogoutScenario();
+            //var testActivityScenario = new TestActivityScenario();
             var testFirmScenario = new TestFirmScenario();
-            var testTicketScenario = new TestTicketScenario();
-            var testProposalScenario = new TestProposalScenario();
+            //var testTicketScenario = new TestTicketScenario();
+            //var testProposalScenario = new TestProposalScenario();
 
+            var scenarios = new List<ScenarioProps>();
             var users = await FetchActiveUsers();
-            var tasks = new List<Task>();
-
             foreach (var user in users)
             {
-                var task = Task.Run(async () =>
-                {
-                    var loginLogoutScenario = testLoginLogoutScenario.Create(user)
-                        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
-                        .WithoutWarmUp();
+                scenarios.Add( testFirmScenario.Create(user)
+                    .WithLoadSimulations(Simulation.Inject(rate: 50, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10)))
+                    .WithoutWarmUp());
 
-                    var activityScenario = testActivityScenario.Create(user)
-                        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
-                        .WithoutWarmUp();
-
-                    var firmScenario = testFirmScenario.Create(user)
-                        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
-                        .WithoutWarmUp();
-
-                    var ticketScenario = testTicketScenario.Create(user)
-                        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
-                        .WithoutWarmUp();
-
-                    var proposalScenario = testProposalScenario.Create(user)
-                        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
-                        .WithoutWarmUp();
-
-                    await RunScenarioWithDelay(loginLogoutScenario, TimeSpan.FromSeconds(3));
-                    await RunScenarioWithDelay(activityScenario, TimeSpan.FromSeconds(3));
-                    await RunScenarioWithDelay(firmScenario, TimeSpan.FromSeconds(3));
-                    await RunScenarioWithDelay(ticketScenario, TimeSpan.FromSeconds(3));
-                    await RunScenarioWithDelay(proposalScenario, TimeSpan.FromSeconds(3));
-                });
-
-                tasks.Add(task);
             }
+           
+            NBomberRunner.RegisterScenarios(scenarios.ToArray())
+                .Run();
+            //var tasks = new List<Task>();
 
-            await Task.WhenAll(tasks);
+            //foreach (var user in users)
+            //{
+            //    var task = Task.Run(async () =>
+            //    {
+            //        var loginLogoutScenario = testLoginLogoutScenario.Create(user)
+            //            .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //            .WithoutWarmUp();
+
+            //        var activityScenario = testActivityScenario.Create(user)
+            //            .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //            .WithoutWarmUp();
+
+            //        var firmScenario = testFirmScenario.Create(user)
+            //            .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //            .WithoutWarmUp();
+
+            //        var ticketScenario = testTicketScenario.Create(user)
+            //            .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //            .WithoutWarmUp();
+
+            //        var proposalScenario = testProposalScenario.Create(user)
+            //            .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //            .WithoutWarmUp();
+
+            //        await RunScenarioWithDelay(loginLogoutScenario, TimeSpan.FromSeconds(3));
+            //        await RunScenarioWithDelay(activityScenario, TimeSpan.FromSeconds(3));
+            //        await RunScenarioWithDelay(firmScenario, TimeSpan.FromSeconds(3));
+            //        await RunScenarioWithDelay(ticketScenario, TimeSpan.FromSeconds(3));
+            //        await RunScenarioWithDelay(proposalScenario, TimeSpan.FromSeconds(3));
+            //    });
+
+            //    tasks.Add(task);
+            //}
+
+            //await Task.WhenAll(tasks);
+
+
+
+            //foreach (var user in users)
+            //{
+            //    var loginLogoutScenario = testLoginLogoutScenario.Create(user)
+            //             .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //             .WithoutWarmUp();
+
+            //    await Task.Delay(TimeSpan.FromSeconds(3));
+
+            //    var activityScenario = testActivityScenario.Create(user)
+            //        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //        .WithoutWarmUp();
+
+            //    await Task.Delay(TimeSpan.FromSeconds(3));
+
+            //    var firmScenario = testFirmScenario.Create(user)
+            //        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //        .WithoutWarmUp();
+
+            //    await Task.Delay(TimeSpan.FromSeconds(3));
+
+            //    var ticketScenario = testTicketScenario.Create(user)
+            //        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //        .WithoutWarmUp();
+
+            //    await Task.Delay(TimeSpan.FromSeconds(3));
+
+            //    var proposalScenario = testProposalScenario.Create(user)
+            //        .WithLoadSimulations(Simulation.Inject(rate: 1, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)))
+            //        .WithoutWarmUp();
+
+            //    await Task.Delay(TimeSpan.FromSeconds(3));
+
+            //    NBomberRunner.RegisterScenarios(loginLogoutScenario, activityScenario, firmScenario, ticketScenario, proposalScenario)
+            //        .Run();
+
+
+            //}
+            
         }
 
         private static void ConfigureServices(IServiceCollection services)
@@ -73,14 +125,14 @@ namespace LogoNBomber
             services.AddHttpClient();
         }
 
-        private static async Task RunScenarioWithDelay(ScenarioProps scenario, TimeSpan delay)
-        {
-            var result = NBomberRunner
-                .RegisterScenarios(scenario)
-                .Run();
+        //private static async Task RunScenarioWithDelay(ScenarioProps scenario, TimeSpan delay)
+        //{
+        //    var result = NBomberRunner
+        //        .RegisterScenarios(scenario)
+        //        .Run();
 
-            await Task.Delay(delay);
-        }
+        //    await Task.Delay(delay);
+        //}
 
         private static async Task<List<UserDto>> FetchActiveUsers()
         {
